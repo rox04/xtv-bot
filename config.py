156984e6c1ec4a9616af584196e7a8617c714aaf
rox04 <112331855+rox04@users.noreply.github.com>
2022-10-03 21:23:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = '5545481518:AAFQcSXCEHPaLiqj8GsYeqQOKyrhBTYKV-0'
-APP_URL = 'https://skorx.herokuapp.com/' + BOT_TOKEN
+BOT_TOKEN = '5662770810:AAE7EmZ0KsmnGUpojDCtyc-b_8vVhGym_5s'
+APP_URL = 'https://appzky.herokuapp.com/' + BOT_TOKEN
